2b0a97d1dbe232c848b76f4ecd87d6e18975b66c
Rindel Subaan <valsubaan@yahoo.com>
2023-04-08 20:00:27 +0800

    Button Component added

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,16 @@
-// import Message from './Message';
-import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+import ButtonSolution from "./components/ButtonSolution";
 
 function App() {
-  return <div><ListGroup /></div>
+  let headingText = "Button Component";
+  return (
+    <div>
+      <h1>{headingText}</h1>
+      <ButtonSolution color="primary" onClick={() => console.log("Clicked")}>
+        Some Text Here
+      </ButtonSolution>
+    </div>
+  );
 }
 
 export default App;
---
+++ react-app/src/components/Button.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function Button() {
+  return (
+    <Fragment>
+      <h1>Button Component</h1>
+      <button className="btn btn-primary">Log in</button>
+    </Fragment>
+  );
+}
+
+export default Button;

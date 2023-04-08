139f0b8a99797d8fdbc5aa706b316c82d0ec4e25
Rindel Subaan <valsubaan@yahoo.com>
2023-04-08 20:00:09 +0800

    deleted files

--- react-app/src/Message.tsx
+++
@@ -1,8 +0,0 @@
-// function based components (recommended approach)
-function Message() {
-    const name = 'World';
-    // JSX or JavaScript XML -> describe UI with html + js
-    return <h1>Hello {name}</h1>;
-}
-
-export default Message;

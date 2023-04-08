ba92610895d6bb88c577304f9fc62a300ba903cb
Rindel Subaan <valsubaan@yahoo.com>
2023-04-08 16:57:39 +0800

    installed bootstrap

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,8 @@
+// function based components (recommended approach)
+function Message() {
+    const name = 'World';
+    // JSX or JavaScript XML -> describe UI with html + js
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;

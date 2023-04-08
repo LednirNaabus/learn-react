a2e068f49ac6665e9bad40fedaf88bfeffd36418
Rindel Subaan <valsubaan@yahoo.com>
2023-04-08 17:53:31 +0800

    Alert interface

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,12 @@
 // import Message from './Message';
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["test1", "test2", "test3"];
-  // separate event handler
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Listahan"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        <b>Warning: </b> Generic error message.
+      </Alert>
     </div>
   );
 }
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -1,5 +1,10 @@
-const Alert = () => {
-  return <div></div>;
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+const Alert = ({ children }: Props) => {
+  return <div className="alert alert-danger">{children}</div>;
 };
 
 export default Alert;
